469f61569c12331cb4f0b216fae40508a68bcad1
ahmed-abbas-1999 <ahmedabbas9912@gmail.com>
2022-04-16 03:02:33 +0300

    first-commit

---
+++ database.js
@@ -0,0 +1,32 @@
+
+
+    var soap =[
+        {"name":"صابون البرتقال","imgpath":"css/products/soap/صابون البرتقال.jpg","price":6000,},
+        {"name":"صابون الارز","imgpath":"css/products/soap/صابون البرتقال.jpg","price":6000,},
+        {"name":"صابون الخيار","imgpath":"css/products/soap/صابون الخيار.jpg","price":6000,},
+        {"name":"صابون الشاي الاخضر","imgpath":"css/products/soap/صابون الشاي الاخضر.jpg","price":6000,},
+        {"name":"صابون الشوفان والافندر والحليب","imgpath":"css/products/soap/صابون الشوفان والافندر والحليب.jpg","price":6000,},
+        {"name":"صابون الفحم","imgpath":"css/products/soap/صابون الفحم.jpg","price":6000,},
+        {"name":"صابون القهوة","imgpath":"css/products/soap/صابون القهوة.jpg","price":6000,},
+        {"name":"صابون الكركم","imgpath":"css/products/soap/صابون الكركم.jpg","price":6000,},]
+
+    var cream=[
+        {"name":"تونر الشاي الاخضر","imgpath":"css/products/self care/تونر الشاي الاخضر.jpg","price":10000},
+        {"name":"كريم الوجه من ايكو","imgpath":"css/products/self care/كريم الوجه من ايكو.jpg","price":15000},
+        {"name":"كريم معالجة جفاف وتشقق الاقدام","imgpath":"css/products/self care/كريم معالجة جفاف وتشقق الاقدام.jpg","price":10000},
+        {"name":"لوشن العسل بعطر التوت البري","imgpath":"css/products/self care/لوشن العسل بعطر التوت البري.jpg","price":10000},
+        {"name":"ماسك الخيار للهالات","imgpath":"css/products/self care/ماسك الخيار للهالات.jpg","price":10000},]
+        
+var all=[  {"name":"صابون البرتقال","imgpath":"css/products/soap/صابون البرتقال.jpg","price":6000,},
+{"name":"صابون الارز","imgpath":"css/products/soap/صابون البرتقال.jpg","price":6000,},
+{"name":"صابون الخيار","imgpath":"css/products/soap/صابون الخيار.jpg","price":6000,},
+{"name":"صابون الشاي الاخضر","imgpath":"css/products/soap/صابون الشاي الاخضر.jpg","price":6000,},
+{"name":"صابون الشوفان والافندر والحليب","imgpath":"css/products/soap/صابون الشوفان والافندر والحليب.jpg","price":6000,},
+{"name":"صابون الفحم","imgpath":"css/products/soap/صابون الفحم.jpg","price":6000,},
+{"name":"صابون القهوة","imgpath":"css/products/soap/صابون القهوة.jpg","price":6000,},
+{"name":"صابون الكركم","imgpath":"css/products/soap/صابون الكركم.jpg","price":6000,},
+{"name":"تونر الشاي الاخضر","imgpath":"css/products/self care/تونر الشاي الاخضر.jpg","price":10000},
+{"name":"كريم الوجه من ايكو","imgpath":"css/products/self care/كريم الوجه من ايكو.jpg","price":15000},
+{"name":"كريم معالجة جفاف وتشقق الاقدام","imgpath":"css/products/self care/كريم معالجة جفاف وتشقق الاقدام.jpg","price":10000},
+{"name":"لوشن العسل بعطر التوت البري","imgpath":"css/products/self care/لوشن العسل بعطر التوت البري.jpg","price":10000},
+{"name":"ماسك الخيار للهالات","imgpath":"css/products/self care/ماسك الخيار للهالات.jpg","price":10000},]
